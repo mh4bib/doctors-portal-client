3c9a82fea6bc922f614001aa9b83e3b35b2ce9ce
mahiuddinhabib <mh.habib137@gmail.com>
2022-05-14 23:21:11 +0600

    appoinment added

--- src/Pages/Home/Appointment.js
+++ src/Pages/Home/Appointment.js
@@ -4,7 +4,7 @@ import doctor from '../../assets/images/doctor.png';
 
 const Appointment = () => {
     return (
-        <div className="hero min-h-screen mt-[60px] mb-[60px] bg-appointment">
+        <div className="hero mt-[60px] mb-[60px] bg-appointment">
                 <div className="hero-content flex-col lg:flex-row-reverse p-4 md:p-0">
                     <div className='text-left text-white'>
                     <h2 className='text-secondary font-bold text-[20px]'>APPOINTMENT</h2>
@@ -12,7 +12,7 @@ const Appointment = () => {
                         <p className="py-6">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
                         <button className="btn btn-primary uppercase text-white font-bold bg-gradient-to-r from-secondary to-primary">Get Started</button>
                     </div>
-                    <img className='hidden md:block w-[606px] ' src={doctor} alt="" />
+                    <img className='hidden md:block w-[606px] mt-[-120px]' src={doctor} alt="" />
                 </div>
             </div>
     );
